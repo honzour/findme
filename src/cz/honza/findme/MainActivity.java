aa4fa308c7c379c77ff686e84894a220fb46ee1d
Jan Němec <jnem6403@seznam.cz>
2014-01-27 22:01:42 +0100

    find and show my position

--- src/cz/honza/findme/MainActivity.java
+++ src/cz/honza/findme/MainActivity.java
@@ -1,18 +1,29 @@
 package cz.honza.findme;
 
+import java.util.Locale;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 
 public class MainActivity extends Activity {
 	
 	LocationManager mlocManager;
 	LocationListener mlocListener;
+	Location mLocation = null;
+	
+	TextView mLongitude;
+	TextView mLatitude;
+	TextView mAltitude;
+	TextView mAccuracy;
 	
 	protected void toast(String text) {
 		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
@@ -24,13 +35,24 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
         
         final View findMeButton = findViewById(R.id.main_find_me);
+        final View openButton = findViewById(R.id.main_open_last);
+        
+        mLongitude = (TextView) findViewById(R.id.main_longitude);
+        mLatitude = (TextView) findViewById(R.id.main_latitude);
+        mAltitude = (TextView) findViewById(R.id.main_altitude);
+        mAccuracy = (TextView) findViewById(R.id.main_accuracy);
         
         mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 	    mlocListener = new LocationListener()
 	    {
 			@Override
 			public void onLocationChanged(Location location) {
-				toast("onLocationChanged");
+				mLongitude.setText(String.valueOf(location.getLongitude()));
+				mLatitude.setText(String.valueOf(location.getLatitude()));
+				mAltitude.setText(String.valueOf(location.getAltitude()));
+				mAccuracy.setText(String.valueOf(location.getAccuracy()));
+			
+				mLocation = location;
 			}
 
 			@Override
@@ -56,6 +78,17 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 			    mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+			}
+		});
+        
+        openButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			    if (mLocation == null)
+			    	return;
+			    String uri = String.format(Locale.ENGLISH, "geo:%f,%f", mLocation.getLatitude(), mLocation.getLongitude());
+			    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(uri));
+			    MainActivity.this.startActivity(intent);
 			}
 		});
     }
