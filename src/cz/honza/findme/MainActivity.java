99614da0a3f9eb0450af4137fdb0d28771560c8c
Jan Nemec <jnemec@acrobits.cz>
2014-01-27 16:41:03 +0100

    add gps listener

--- src/cz/honza/findme/MainActivity.java
+++ src/cz/honza/findme/MainActivity.java
@@ -1,13 +1,68 @@
 package cz.honza.findme;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 
 public class MainActivity extends Activity {
+	
+	LocationManager mlocManager;
+	LocationListener mlocListener;
+	
+	protected void toast(String text) {
+		Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+	}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main);
+        
+        final View findMeButton = findViewById(R.id.main_find_me);
+        
+        mlocManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+	    mlocListener = new LocationListener()
+	    {
+			@Override
+			public void onLocationChanged(Location location) {
+				toast("onLocationChanged");
+			}
+
+			@Override
+			public void onProviderDisabled(String provider) {
+				toast("onProviderDisabled");
+			}
+
+			@Override
+			public void onProviderEnabled(String provider) {
+				toast("onProviderEnabled");
+			}
+
+			@Override
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				toast("onStatusChanged");
+			}
+	    	
+	    };
+
+        
+        findMeButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+			    mlocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mlocListener);
+			}
+		});
     }
+
+	@Override
+	protected void onDestroy() {
+		mlocManager.removeUpdates(mlocListener);
+		super.onDestroy();
+	}
 }
