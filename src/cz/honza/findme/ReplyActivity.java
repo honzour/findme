2e5d9b73833883de17d05c24d22ad443346e53d1
Jan Němec <jnem6403@seznam.cz>
2014-02-12 06:31:59 +0100

    reply spinner

--- src/cz/honza/findme/ReplyActivity.java
+++ src/cz/honza/findme/ReplyActivity.java
@@ -1,6 +1,8 @@
 package cz.honza.findme;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.app.Activity;
 
 public class ReplyActivity extends Activity {
@@ -9,5 +11,12 @@ public class ReplyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reply);
+        Spinner mode = (Spinner) findViewById(R.id.reply_mode);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.reply_mode, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
+        mode.setAdapter(adapter);
     }
 }
