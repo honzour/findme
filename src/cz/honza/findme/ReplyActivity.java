d642e2178eb8ec9eb6edabfde349c581684373d0
Jan Nemec <jnemec@acrobits.cz>
2014-02-17 09:25:45 +0100

    reply - basic configuration GUI

--- src/cz/honza/findme/ReplyActivity.java
+++ src/cz/honza/findme/ReplyActivity.java
@@ -5,6 +5,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.app.Activity;
 
 public class ReplyActivity extends Activity {
@@ -13,29 +14,44 @@ public class ReplyActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.reply);
+        
+        final TextView modeInfo = (TextView) findViewById(R.id.reply_mode_info);
         Spinner mode = (Spinner) findViewById(R.id.reply_mode);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.reply_mode, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
                 
         mode.setAdapter(adapter);
-        
         mode.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
 			@Override
 			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				// TODO Auto-generated method stub
-				
+					int item, long arg3) {
+
+				switch (item)
+				{
+				case 0:
+					modeInfo.setText(R.string.reply_mode_reply_all);
+					break;
+				case 1:
+					modeInfo.setText(R.string.reply_mode_do_not_reply);
+					break;
+				case 2:
+					modeInfo.setText(R.string.reply_mode_reply_secret);
+					break;
+				default:
+					modeInfo.setText("");
+					break;
+				}
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> arg0) {
-				// TODO Auto-generated method stub
 				
 			}
         	
 		});
+
         mode.setSelection(1);
         
     }
