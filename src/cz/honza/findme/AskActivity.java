9d2ab6d62ecd763d7c24fd182764fcde0802d65b
Jan Nemec <jnemec@acrobits.cz>
2014-02-19 13:43:03 +0100

    ask

--- src/cz/honza/findme/AskActivity.java
+++ src/cz/honza/findme/AskActivity.java
@@ -1,6 +1,10 @@
 package cz.honza.findme;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class AskActivity extends Activity {
@@ -9,5 +13,19 @@ public class AskActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.ask);
+        
+        final View askSms = findViewById(R.id.ask_sms);
+        askSms.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				final EditText number = (EditText) findViewById(R.id.ask_number);
+				final CheckBox attach = (CheckBox) findViewById(R.id.ask_attach);
+				
+				String url = "findme:ask?v=1&n=" + number.getText() + "&a=" + (attach.isChecked() ? '1' : '0') + " ";
+				
+				Toast.makeText(AskActivity.this, url, Toast.LENGTH_LONG).show();
+				finish();
+			}
+		});
     }
 }
