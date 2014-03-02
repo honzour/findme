8e9d58a2126ec9a0c358f23f03175727a0912854
Jan Němec <jnem6403@seznam.cz>
2014-03-02 06:42:51 +0100

    FindMeUrl split into more files

---
+++ src/cz/honza/findme/Handling.java
@@ -0,0 +1,95 @@
+package cz.honza.findme;
+
+import java.util.Calendar;
+
+import android.content.Intent;
+import android.location.Location;
+import android.net.Uri;
+
+public class Handling {
+	public static void handleUri(final String from, Uri uri)
+	{
+		String host = uri.getHost();
+		if (host.equals(FindMeUrl.ACTION_ASK))
+		{
+			tryToHandlePosition(from, uri, null);
+			Position.findMyPosition(300, new Position.Callback() {
+				
+				@Override
+				public void onTimeout() {
+					sendReply(from, null);					
+				}
+				
+				@Override
+				public void onMyPositionFound(Location location) {
+					sendReply(from, location);
+				}
+				
+				@Override
+				public void onError(int errorCode) {
+					sendReply(from, null);
+				}
+			});
+			return;
+		}
+		
+		if (host.equals(FindMeUrl.ACTION_REPLY))
+		{
+			tryToHandlePosition(from, uri, new Runnable() {
+				@Override
+				public void run() {
+					Util.toast(R.string.reply_without_location);					
+				}
+			});
+			return;
+		}
+	}
+	
+	public static void handleIncomingPosition(String action, String from, double lon, double lat)
+	{
+		int actionResource = R.string.incoming_position;
+		if (action.equals(FindMeUrl.ACTION_ASK))
+			actionResource = R.string.incoming_position_ask;
+		if (action.equals(FindMeUrl.ACTION_REPLY))
+			actionResource = R.string.incoming_position_reply;
+		Intent intent = new Intent(FindMeApplication.sInstance, ShowPositionActivity.class);
+		intent.putExtra(ShowPositionActivity.EXTRA_CAPTION, FindMeApplication.sInstance.getResources().getString(actionResource));
+		intent.putExtra(ShowPositionActivity.EXTRA_LON, lon);
+		intent.putExtra(ShowPositionActivity.EXTRA_LAT, lat);
+		intent.putExtra(ShowPositionActivity.EXTRA_NUMBER, from);
+		intent.putExtra(ShowPositionActivity.EXTRA_TIME, Calendar.getInstance());
+		intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+		FindMeApplication.sInstance.startActivity(intent);
+	}
+	
+	protected static void tryToHandlePosition(String from, Uri uri, Runnable onError)
+	{
+		String host = uri.getHost();
+		String lon = uri.getQueryParameter(String.valueOf(FindMeUrl.CHAR_LONGITUDE));
+		String lat = uri.getQueryParameter(String.valueOf(FindMeUrl.CHAR_LATITUDE));
+		if (lon != null && lat != null)
+		{
+			try
+			{
+				double lond = Double.valueOf(lon);
+				double latd = Double.valueOf(lat);
+				handleIncomingPosition(host, from, lond, latd);
+				return;
+			}
+			catch (NumberFormatException e)
+			{
+				// ignore
+			}
+		}
+		if (onError != null)
+		{
+			// TODO
+		}
+	}
+	
+	protected static void sendReply(String to, Location location)
+	{
+		String message = FindMeUrl.createReplyUrl(true, location);
+		Util.sendSMS(to, message);
+	}
+}
