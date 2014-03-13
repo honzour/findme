263135923f7071e3759563449431f6a02be90cca
Jan Němec <jnem6403@seznam.cz>
2014-03-13 06:03:38 +0100

    error activity

--- src/cz/honza/findme/ShowErrorActivity.java
+++ src/cz/honza/findme/ShowErrorActivity.java
@@ -1,7 +1,9 @@
 package cz.honza.findme;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ShowErrorActivity extends Activity {
 	public static final String EXTRA_CAPTION = "EXTRA_CAPTION";
@@ -9,7 +11,28 @@ public class ShowErrorActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.show_error);
+		
+		Intent intent = getIntent();
+		Bundle extras = intent.getExtras();
+		
+		if (extras == null)
+			return;
+		
+		final String caption = extras.getString(EXTRA_CAPTION);
+		if (caption != null) 
+		{
+			setTitle(caption);
+			TextView captionView = (TextView)findViewById(R.id.caption);
+			captionView.setText(caption);
+		}
+		
+		final String text = extras.getString(EXTRA_TEXT);
+		if (text != null) 
+		{
+			TextView textView = (TextView)findViewById(R.id.text);
+			textView.setText(text);
+		}
 	}
-	
-	
 }
