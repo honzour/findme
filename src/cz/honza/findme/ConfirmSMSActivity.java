e2d11ea5d70907c9fd0d5ada3f1938757f46a64a
Jan Nemec <jnemec@acrobits.cz>
2014-03-14 12:08:02 +0100

    confirm sms

--- src/cz/honza/findme/ConfirmSMSActivity.java
+++ src/cz/honza/findme/ConfirmSMSActivity.java
@@ -1,9 +1,17 @@
 package cz.honza.findme;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class ConfirmSMSActivity extends Activity {
 	public static final String EXTRA_TO = "EXTRA_TO"; 
 	public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
 	public static final String EXTRA_AUTOREPLY = "EXTRA_AUTOREPLY";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+	
+	
 }
