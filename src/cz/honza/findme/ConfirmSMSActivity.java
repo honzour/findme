1b3da7542a09b8e7ed9aabd4579b682c47ba0a36
Jan Nemec <jnemec@acrobits.cz>
2014-03-14 15:53:34 +0100

    confirm sms

--- src/cz/honza/findme/ConfirmSMSActivity.java
+++ src/cz/honza/findme/ConfirmSMSActivity.java
@@ -2,6 +2,7 @@ package cz.honza.findme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ConfirmSMSActivity extends Activity {
 	public static final String EXTRA_TO = "EXTRA_TO"; 
@@ -12,6 +13,32 @@ public class ConfirmSMSActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.confirm_sms);
+		
+		View dontSend = findViewById(R.id.do_not_send);
+		dontSend.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
+		
+		Bundle extras = getIntent().getExtras(); 
+		
+		if (extras == null)
+			return;
+		
+		final String to = extras.getString(EXTRA_TO);
+		final String message = extras.getString(EXTRA_MESSAGE);
+		final boolean autoreply = extras.getBoolean(EXTRA_AUTOREPLY);
+		
+		View send = findViewById(R.id.do_not_send);
+		send.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				Util.sendSMS(to, message);
+				finish();
+			}
+		});
 	}
 	
 	
