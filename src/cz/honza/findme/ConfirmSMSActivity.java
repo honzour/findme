2892509fa451d14dec666101b5779c24088614fc
Jan Nemec <jnemec@acrobits.cz>
2014-03-14 15:40:41 +0100

    dimen and color

--- src/cz/honza/findme/ConfirmSMSActivity.java
+++ src/cz/honza/findme/ConfirmSMSActivity.java
@@ -11,6 +11,7 @@ public class ConfirmSMSActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.confirm_sms);
 	}
 	
 	
