2d7c7cea9fc6c366dde7b5c1ddd4a0c88c8e7164
Jan Nemec <jnemec@acrobits.cz>
2014-03-19 14:23:30 +0100

    fixed autoreply

--- src/cz/honza/findme/ConfirmSMSActivity.java
+++ src/cz/honza/findme/ConfirmSMSActivity.java
@@ -2,7 +2,9 @@ package cz.honza.findme;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 public class ConfirmSMSActivity extends Activity {
@@ -10,10 +12,19 @@ public class ConfirmSMSActivity extends Activity {
 	public static final String EXTRA_MESSAGE = "EXTRA_MESSAGE";
 	public static final String EXTRA_AUTOREPLY = "EXTRA_AUTOREPLY";
 	
+	public static final String STATE_PROGRESS = "STATE_PROGRESS";
+	
+	private Handler mHandler;
+	private Runnable mAutoreplyStep = null;
+	private int mAutoreplyTimer = 0;
+	private static final int STEP_MS = 1000;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.confirm_sms);
+		
+		mAutoreplyTimer = (savedInstanceState == null ? 0 : savedInstanceState.getInt(STATE_PROGRESS, 0));
 		
 		View dontSend = findViewById(R.id.do_not_send);
 		dontSend.setOnClickListener(new View.OnClickListener() {
@@ -38,9 +49,34 @@ public class ConfirmSMSActivity extends Activity {
 		
 		final boolean autoreply = extras.getBoolean(EXTRA_AUTOREPLY);
 		
-		View progress = findViewById(R.id.progress);
-		if (!autoreply)
+		final ProgressBar progress = (ProgressBar)findViewById(R.id.progress);
+
+		if (autoreply)
+		{
+			mHandler = new Handler();
+			mAutoreplyStep =  new Runnable() {
+				@Override
+				public void run() {
+					if (mAutoreplyTimer <= Settings.autoreplyTimeout)
+					{
+						mHandler.postDelayed(mAutoreplyStep, STEP_MS);
+						progress.setProgress(++mAutoreplyTimer);
+					}
+					else
+					{
+						Util.sendSMS(to, message, true);
+						finish();
+					}
+				}
+			};
+			progress.setMax(Settings.autoreplyTimeout);
+			progress.setProgress(mAutoreplyTimer);
+			mHandler.postDelayed(mAutoreplyStep, STEP_MS);
+		}
+		else
+		{
 			progress.setVisibility(View.GONE);
+		}
 		
 		View send = findViewById(R.id.send);
 		send.setOnClickListener(new View.OnClickListener() {
@@ -50,7 +86,33 @@ public class ConfirmSMSActivity extends Activity {
 				finish();
 			}
 		});
+		
+		
 	}
+	
+	
+
+	@Override
+	protected void onDestroy() {
+		if (mHandler != null)
+			mHandler.removeCallbacksAndMessages(null);
+		super.onDestroy();
+	}
+
+
+
+	@Override
+	protected void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		outState.putInt(STATE_PROGRESS, mAutoreplyTimer);
+	}
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		
+	}
+	
 	
 	
 }
--- src/cz/honza/findme/Settings.java
+++ src/cz/honza/findme/Settings.java
@@ -2,4 +2,5 @@ package cz.honza.findme;
 
 public class Settings {
 	public static int gpsTimeout = 300;
+	public static int autoreplyTimeout = 60;
 }
