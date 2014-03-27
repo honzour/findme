d6ef2e673a09cecd140b3c71608a8e85d40c6469
Jan Němec <jnem6403@seznam.cz>
2014-03-27 04:48:25 +0100

    empty history

---
+++ src/cz/honza/findme/history/HistoryActivity.java
@@ -0,0 +1,14 @@
+package cz.honza.findme.history;
+
+import cz.honza.findme.FindmeActivity;
+import cz.honza.findme.R;
+import android.os.Bundle;
+
+public class HistoryActivity extends FindmeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.history);
+    }
+}
