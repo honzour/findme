98af1428128db145661dda546567fbd42d26a2aa
Jan Němec <jnem6403@seznam.cz>
2014-03-29 06:40:44 +0100

    history (still with errors)

--- src/cz/honza/findme/history/HistoryActivity.java
+++ src/cz/honza/findme/history/HistoryActivity.java
@@ -1,8 +1,11 @@
 package cz.honza.findme.history;
 
+import cz.honza.findme.FindMeApplication;
 import cz.honza.findme.FindmeActivity;
 import cz.honza.findme.R;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 
 public class HistoryActivity extends FindmeActivity {
 
@@ -10,5 +13,14 @@ public class HistoryActivity extends FindmeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history);
+        Cursor c = FindMeApplication.sDbHelper.select();
+        if (c != null)
+        {
+        	do {
+        		HistoryItem item = HistoryItem.fromCursor(c);
+        		Log.i("es em es", item.mNumber + " " + item.mSms);
+        	} while (c.moveToNext());
+        	c.close();
+        }
     }
 }
